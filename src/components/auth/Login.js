cf4637d0e91b2fee977d8e7970e0e4f88258d926
Hewson Schulz <hewsonschulz@gmail.com>
2023-12-12 13:13:53 -0600

    login tweaks
    fixed an async bug,
    fixed email not being converted to lowercase

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,61 +1,60 @@
-import React, { useState } from "react"
-import { Link } from "react-router-dom"
-import { useNavigate } from "react-router-dom"
-import "./Login.css"
-import { getUserByEmail } from "../../services/userService"
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import './Login.css'
+import { getUserByEmail } from '../../services/userService'
 
 export const Login = () => {
-  const [email, set] = useState("")
+  const [email, setEmail] = useState('')
   const navigate = useNavigate()
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault()
 
-    return getUserByEmail(email).then((foundUsers) => {
-      if (foundUsers.length === 1) {
-        const user = foundUsers[0]
-        localStorage.setItem(
-          "learning_user",
-          JSON.stringify({
-            id: user.id,
-          })
-        )
+    const foundUsers = await getUserByEmail(email.toLowerCase())
+    if (foundUsers.length === 1) {
+      const user = foundUsers[0]
+      localStorage.setItem(
+        'learning_user',
+        JSON.stringify({
+          id: user.id,
+        })
+      )
 
-        navigate("/")
-      } else {
-        window.alert("Invalid login")
-      }
-    })
+      navigate('/')
+    } else {
+      window.alert('Invalid login')
+    }
   }
 
   return (
-    <main className="auth-container">
+    <main className='auth-container'>
       <section>
-        <form className="auth-form" onSubmit={handleLogin}>
-          <h1 className="header">Learning Moments</h1>
+        <form className='auth-form' onSubmit={handleLogin}>
+          <h1 className='header'>Learning Moments</h1>
           <h2>Please sign in</h2>
-          <fieldset className="auth-fieldset">
+          <fieldset className='auth-fieldset'>
             <div>
               <input
-                type="email"
+                type='email'
                 value={email}
-                className="auth-form-input"
-                onChange={(evt) => set(evt.target.value)}
-                placeholder="Email address"
+                className='auth-form-input'
+                onChange={(evt) => setEmail(evt.target.value)}
+                placeholder='Email address'
                 required
                 autoFocus
               />
             </div>
           </fieldset>
-          <fieldset className="auth-fieldset">
+          <fieldset className='auth-fieldset'>
             <div>
-              <button type="submit">Sign in</button>
+              <button type='submit'>Sign in</button>
             </div>
           </fieldset>
         </form>
       </section>
-      <section className="register-link">
-        <Link to="/register">Not a member yet?</Link>
+      <section className='register-link'>
+        <Link to='/register'>Not a member yet?</Link>
       </section>
     </main>
   )

3a31014f49cb5ca51a1124b5b752b9a27292189f
Hewson Schulz <hewsonschulz@gmail.com>
2023-12-24 16:15:48 -0600

    styling
    added navbar item becoming highlighted when at corresponding url

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,42 +1,67 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import './NavBar.css'
 
 export const NavBar = ({ loggedInUser }) => {
-    const navigate = useNavigate()
+	const navigate = useNavigate()
+	const url = useLocation().pathname
 
-    return <ul className='navbar'>
+	return (
+		<ul className='navbar'>
+			<li className='navbar-item'>
+				<Link to='/' className='navbar-link' id={url === '/' ? 'selected' : ''}>
+					All Posts
+				</Link>
+			</li>
 
-        <li className='navbar-item'>
-            <Link to='/' className='navbar-link'>All Posts</Link>
-        </li>
+			<li className='navbar-item'>
+				<Link to='/new' className='navbar-link' id={url === '/new' ? 'selected' : ''}>
+					New Post
+				</Link>
+			</li>
 
-        <li className='navbar-item'>
-            <Link to='/new' className='navbar-link'>New Post</Link>
-        </li>
+			<li className='navbar-item'>
+				<Link to='/created' className='navbar-link' id={url === '/created' ? 'selected' : ''}>
+					My Posts
+				</Link>
+			</li>
 
-        <li className='navbar-item'>
-            <Link to='/created' className='navbar-link'>My Posts</Link>
-        </li>
+			<li className='navbar-item'>
+				<Link to='/liked' className='navbar-link' id={url === '/liked' ? 'selected' : ''}>
+					Favorites
+				</Link>
+			</li>
 
-        <li className='navbar-item'>
-            <Link to='/liked' className='navbar-link'>Favorites</Link>
-        </li>
+			<li className='navbar-item'>
+				<Link
+					to={`/profile/${loggedInUser.id}`}
+					className='navbar-link'
+					id={
+						url === `/profile/${loggedInUser.id}` || url === `/profile/edit/${loggedInUser.id}`
+							? 'selected'
+							: ''
+					}
+				>
+					Profile
+				</Link>
+			</li>
 
-        <li className='navbar-item'>
-            <Link to={`/profile/${loggedInUser.id}`} className='navbar-link'>Profile</Link>
-        </li>
-
-        {localStorage.getItem('learning_user') ? (
-            <li className='navbar-item navbar-logout'>
-                <Link to='' onClick={() => {
-                    localStorage.removeItem('learning_user')
-                    navigate('/login', { replace: true })
-                }} className='navbar-link'>
-                    Logout
-                </Link>
-            </li>
-        ) : ('')}
-
-    </ul>
+			{localStorage.getItem('learning_user') ? (
+				<li className='navbar-item navbar-logout'>
+					<Link
+						to=''
+						onClick={() => {
+							localStorage.removeItem('learning_user')
+							navigate('/login', { replace: true })
+						}}
+						className='navbar-link'
+					>
+						Logout
+					</Link>
+				</li>
+			) : (
+				''
+			)}
+		</ul>
+	)
 }
